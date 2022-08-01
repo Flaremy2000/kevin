e0ede9ceae1c82900a31cd604b0ef335f7b4c15b
Flaremy <jeremyponce2016@gmail.com>
2022-07-31 22:11:31 -0500

    Se culmino el sistema de configuracion externa de los micros y se agrego los cruds del sistema de bandadas

---
+++ guardartemp.php
@@ -0,0 +1,38 @@
+<?php
+
+include "visualizador.php";
+
+date_default_timezone_set("America/Guayaquil");
+$date = date("Y-m-d H:i:s");
+
+$registrador = new visualizador();
+
+$temp_max = 0;
+$temp_min = 0;
+$ventilador = 0;
+$luces = 0;
+$c = $registrador->configuracion_temperatura();
+
+
+if($c->rowCount()){
+    while($row = $c->fetch(PDO::FETCH_ASSOC)){
+        $temp_max = $row['temp_max'];
+        $temp_min = $row['temp_min'];
+        $ventilador = $row['ventiladores'];
+        $luces = $row['luces'];
+    }
+}
+
+echo "v=".$temp_max.",".$temp_min.",".$ventilador.",".$luces;
+
+$temp1 = $_GET['tempp'];
+$hum1 = $_GET['hump'];
+$vent = $_GET['vent'];
+$luz = $_GET['luces'];
+
+
+$registrador->registrar_temperatura($temp1, $hum1, $vent, $luz, $date);
+
+
+
+?>
